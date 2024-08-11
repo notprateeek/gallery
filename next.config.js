f77f9ceb0a935baedafaaeb6db39728b3533ae56
Prateek <prateek.bhandula@cropsly.com>
2024-08-11 16:16:43 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/my-account/**',
+      },
+    ],
+    domains: ['res.cloudinary.com'],
+  },
+}
